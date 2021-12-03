8bef89ddbe4700bf67ff05e0e2aed00eb2437584
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-03 19:03:14 +0530

    [ADD] allauth in installed app, [ADD] allauth config

--- AppointmentBookingSystem/AppointmentBookingSystem/settings.py
+++ AppointmentBookingSystem/AppointmentBookingSystem/settings.py
@@ -41,7 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
 ]
+
+SITE_ID = 1
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -70,6 +80,14 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 WSGI_APPLICATION = 'AppointmentBookingSystem.wsgi.application'
 
