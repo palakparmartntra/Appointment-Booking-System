f7ec6891add56f42678c0e9ac44e7ea19703171e
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-07 13:18:35 +0530

    [ADD] for role, specialties and gender constants

---
+++ AppointmentBookingSystem/accounts/constants.py
@@ -0,0 +1,14 @@
+ROLE = (
+    ("Doctor", "Doctor"),
+    ("Patient", "Patient"),
+)
+GENDER = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
+SPECIALITIES = (
+    ('MS', 'MS'),
+    ('MD', 'MD'),
+    ('Dentist', 'Dentist'),
+    ('Physiotherapist', 'Physiotherapist'),
+)
