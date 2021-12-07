3de21db3e087de06d44f59322aa54baff2653e93
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-07 13:17:42 +0530

    [ADD] custom user model

--- AppointmentBookingSystem/accounts/models.py
+++ AppointmentBookingSystem/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from .constants import ROLE, GENDER, SPECIALITIES
 # Create your models here.
+
+
+class User(AbstractUser):
+    """Abstract User table"""
+    contact = PhoneNumberField()
+    birthdate = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER, blank=True, null=True)
+    role = models.CharField(
+        max_length=50, choices=ROLE, null=True, blank=True, default=ROLE[1][1]
+    )
+    specialities = models.CharField(max_length=20, choices=SPECIALITIES, blank=True, null=True)
+    photo = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
+    def __str__(self):
+        return str(self.username)
