ca42f1369a8f2d117b6830d7521f72596715bc70
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-07 14:52:54 +0530

    [ADD] static setting of media file, [ADD] template view

--- AppointmentBookingSystem/AppointmentBookingSystem/urls.py
+++ AppointmentBookingSystem/AppointmentBookingSystem/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-]
+
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
