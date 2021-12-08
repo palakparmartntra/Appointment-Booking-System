0de28a202cbb5e19a503c8db2a888b6b0fbb1595
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-08 16:55:32 +0530

    [ADD] path for doctor profile, update view

--- AppointmentBookingSystem/accounts/urls.py
+++ AppointmentBookingSystem/accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
-
-from accounts.views import AddDoctorView
+from django.urls import path
+from accounts.views import AddDoctorView, DoctorProfileView, UpdateDoctorView
 
 urlpatterns = [
     path("add_doctor/", AddDoctorView.as_view(), name="add_doctor"),
+    path('profile/<pk>/', DoctorProfileView.as_view(), name='profile'),
+    path('update/<pk>/',UpdateDoctorView.as_view(), name='update'),
 ] 
