91d02d82b687b3fe698bd2b541bfb92ab7bafb11
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-08 11:32:18 +0530

    [ADD] accounts app url path

--- AppointmentBookingSystem/AppointmentBookingSystem/urls.py
+++ AppointmentBookingSystem/AppointmentBookingSystem/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-
+    path('accounts/', include('accounts.urls')),
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
