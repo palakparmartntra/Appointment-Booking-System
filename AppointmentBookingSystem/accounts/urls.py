c66d42e0535de79a821ddc532f71f8454dff05c5
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-09 12:27:03 +0530

    [ADD] change password path

--- AppointmentBookingSystem/accounts/urls.py
+++ AppointmentBookingSystem/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from accounts.views import AddDoctorView, DoctorProfileView, UpdateDoctorView
+from accounts.views import AddDoctorView, DoctorProfileView, UpdateDoctorView, ChangeDoctorPasswordView
 
 urlpatterns = [
     path("add_doctor/", AddDoctorView.as_view(), name="add_doctor"),
     path('profile/<pk>/', DoctorProfileView.as_view(), name='profile'),
     path('update/<pk>/',UpdateDoctorView.as_view(), name='update'),
+    path('change_password/', ChangeDoctorPasswordView.as_view(), name='change_password'),
+
 ] 
