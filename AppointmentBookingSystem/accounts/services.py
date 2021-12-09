3b02e47440ac0a22d52bd374eafceebd4a8f5e67
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-09 11:40:45 +0530

    [ADD] for store id in emailaddress model

---
+++ AppointmentBookingSystem/accounts/services.py
@@ -0,0 +1,12 @@
+from allauth.account.models import EmailAddress
+
+class Account():
+    def set_email(self, userdata, *args, **kwargs):
+        """
+        store the data in EmailAddress model of created user using FK.
+        """
+        userd = EmailAddress.objects.get_or_create(user=userdata,
+                                                   email=userdata.email,
+                                                   verified=True,
+                                                   primary=True)
+        return userd
