9d074c59650f08564846bdb4015f481a9ed5f29b
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-10 17:23:25 +0530

    [ADD] included appointment app path

--- AppointmentBookingSystem/AppointmentBookingSystem/urls.py
+++ AppointmentBookingSystem/AppointmentBookingSystem/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('appointments/', include('appointments.urls') ),
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
