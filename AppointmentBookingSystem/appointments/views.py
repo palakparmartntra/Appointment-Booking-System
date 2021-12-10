3bce7eaa85c6fbc801ff5e78a3b60d29eadaeaab
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-10 18:57:22 +0530

    [ADD] appointment view

--- AppointmentBookingSystem/appointments/views.py
+++ AppointmentBookingSystem/appointments/views.py
@@ -3,12 +3,8 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from accounts.constants import ROLE, SPECIALITIES
 from accounts.models import User
-from django.views.generic import ListView
 from django.db.models import Count
-from django.contrib.auth.mixins import LoginRequiredMixin
-import logging
-from django.http import JsonResponse
-import copy
+
 # Create your views here.
 
 
@@ -18,6 +14,11 @@ class SpecialitiesView(View):
     def get(self, request):
         doctors = User.objects.filter(role=ROLE[0][0])
         specialities =  User.objects.exclude(specialities=None).values('specialities').annotate(Count('specialities'))
-        print(specialities)
         data = {"specialities": specialities, "doctors":doctors}
         return render(request, "specialities.html", data)
+
+
+class BookAppointment(View):
+
+    def get(self, request):
+        return render(request, 'specialities.html')
