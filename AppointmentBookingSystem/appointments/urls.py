a21282bffd3d27b952e939ed47ea3689cc4d5317
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-13 19:16:11 +0530

    [ADD] booking path for book appointment

--- AppointmentBookingSystem/appointments/urls.py
+++ AppointmentBookingSystem/appointments/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import SpecialitiesView
+from .views import SpecialitiesView, BookAppointment
 urlpatterns = [
     path('specialities/', SpecialitiesView.as_view(), name="specialities" ),
+    path('booking/', BookAppointment.as_view(), name='booking'),
 ] 
