d341755dc3b1f7372b62f682449ce710caf2aa63
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-13 19:16:35 +0530

    [ADD] booking view

--- AppointmentBookingSystem/appointments/views.py
+++ AppointmentBookingSystem/appointments/views.py
@@ -1,9 +1,12 @@
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
+from django.views.generic import CreateView
 from accounts.constants import ROLE, SPECIALITIES
 from accounts.models import User
 from django.db.models import Count
+from appointments.forms import AppointmentForm
+
 
 # Create your views here.
 
@@ -19,6 +22,18 @@ class SpecialitiesView(View):
 
 
 class BookAppointment(View):
-
     def get(self, request):
-        return render(request, 'specialities.html')
+        form = AppointmentForm()
+        print(form)
+        return render(request, 'account/_application_form.html', {'form': form})    
+
+    def post(self, request):
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment_data = form.save(commit=False)
+            appointment_data.patient = self.request.user
+            appointment_data.save()
+            return redirect('index')
+        else:
+            form = AppointmentForm()
+            return render(request, 'account/_application_form.html', {'form': form}) 
