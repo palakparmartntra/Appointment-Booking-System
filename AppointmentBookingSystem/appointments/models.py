5977465e3265ae0853fd6fcd7cb0f2792531bb9d
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-13 19:15:48 +0530

    [ADD] null and blank true in appointment model

--- AppointmentBookingSystem/appointments/models.py
+++ AppointmentBookingSystem/appointments/models.py
@@ -11,8 +11,8 @@ STATUS=(
 class Appointment(models.Model):
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient')
     doctor = models.ForeignKey(User, on_delete=models.CASCADE,  related_name='doctor')
-    appoint_date = models.DateTimeField()
-    update_date = models.DateTimeField()
-    status = models.CharField(max_length=50, choices=STATUS, null=True, blank=True)
-    description = models.TextField()
+    appoint_date = models.DateTimeField(null=True, blank=True)
+    update_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=50, choices=STATUS, null=True, blank=True, default=STATUS[0][0])
+    description = models.TextField(null=True, blank=True)
 
