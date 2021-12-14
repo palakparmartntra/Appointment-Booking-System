4102de22ae85eef479f7b9a7ac1eafe3ec6bacdb
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-14 19:21:53 +0530

    [ADD] path for appointment list

--- AppointmentBookingSystem/appointments/urls.py
+++ AppointmentBookingSystem/appointments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SpecialitiesView, BookAppointment
+from .views import SpecialitiesView, BookAppointment, AppointmentListView
 urlpatterns = [
     path('specialities/', SpecialitiesView.as_view(), name="specialities" ),
-    path('booking/', BookAppointment.as_view(), name='booking'),
+    path('booking/', BookAppointment.as_view(), name="booking"),
+    path('appointment_list/', AppointmentListView.as_view(), name="appointment_list"),
 ] 
