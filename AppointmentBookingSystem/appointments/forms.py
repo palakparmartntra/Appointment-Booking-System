83077e823e715b6acfea313f96b1ba1fd4d2c0f4
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-14 19:19:46 +0530

    [MODIFIED] widgets and init method in appointmentform

--- AppointmentBookingSystem/appointments/forms.py
+++ AppointmentBookingSystem/appointments/forms.py
@@ -1,12 +1,31 @@
+from accounts.models import User
 from appointments.models import Appointment
 from django import forms
-from accounts.constants import GENDER, SPECIALITIES
+from accounts.constants import GENDER, ROLE, SPECIALITIES
 from django.contrib.auth.forms import PasswordChangeForm
-
+from django.contrib.admin import widgets                                       
+import datetime
 
 class AppointmentForm(forms.ModelForm):
     """ This form for create appointment ."""
-    
+
     class Meta:
         model = Appointment
         fields = ('doctor', 'appoint_date', 'description')
+        widgets = {
+            'appoint_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        super(AppointmentForm, self).__init__(*args, **kwargs)
+        self.fields['doctor'].queryset = User.objects.filter(role=ROLE[0][0])
+
+    # def clean(self):
+    #     appoint_date = self.cleaned_data['appoint_date']
+    #     import code; code.interact(local=dict(globals(), **locals()))
+
+    #     if appoint_date < datetime.datetime.today():
+    #         raise forms.ValidationError("The date cannot be in the past!")
+    #     return self.cleaned_data
+                                                
