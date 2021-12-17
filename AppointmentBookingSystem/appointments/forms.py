6edd2a5c16e90de88a03975cdcd5028bed72b71b
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-17 16:56:40 +0530

    [ADD] specialities field in book appointment

--- AppointmentBookingSystem/appointments/forms.py
+++ AppointmentBookingSystem/appointments/forms.py
@@ -1,20 +1,25 @@
 from accounts.models import User
 from appointments.models import Appointment
 from django import forms
-from accounts.constants import ROLE
+from accounts.constants import ROLE, SPECIALITIES
 import datetime
 
 class AppointmentForm(forms.ModelForm):
     """ This form for create appointment ."""
-
+    # specialities = forms.ChoiceField(choice= SPECIALITIES)
+    specialities = forms.ChoiceField(choices=SPECIALITIES)
     class Meta:
         model = Appointment
-        fields = ('doctor', 'appoint_date', 'description')
+        fields = ('specialities', 'doctor', 'appoint_date', 'description')
         widgets = {
             'appoint_date': forms.DateTimeInput(attrs={'type': 'datetime-local','class':'datetimepicker', 'min':datetime.datetime.today()}),
         }
 
-
     def __init__(self, *args, **kwargs):
+        # self.request = kwargs.pop("request")
         super(AppointmentForm, self).__init__(*args, **kwargs)
         self.fields['doctor'].queryset = User.objects.filter(role=ROLE[0][0])
+        if 'specialities' in self.data:   
+            spec = self.data.get('specialities')
+            doc = User.objects.filter(role=ROLE[0][0])
+            self.fields['doctor'].queryset = doc.filter(specialities=spec)
