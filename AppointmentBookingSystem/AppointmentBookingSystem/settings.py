3fce2c2f397ccb87ce9d387ad7878c2306f9bc6f
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-20 12:31:34 +0530

    [ADD] message tags color

--- AppointmentBookingSystem/AppointmentBookingSystem/settings.py
+++ AppointmentBookingSystem/AppointmentBookingSystem/settings.py
@@ -18,7 +18,15 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from django.contrib.messages import constants as messages
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
